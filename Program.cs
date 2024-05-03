fbe173d8d1039fc4dc0fafca4d795407e09cef64
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:28:31 -0300

    Projeto Concluido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.AccessControl;
+
+string Senhadigitada;
+string Senha = "1234abcd";
+
+Console.Write("Olá usuario, Digite sua Senha de acesso...: ");
+Senhadigitada = Console.ReadLine()!;
+
+if (Senhadigitada == Senha)
+    {
+        Console.WriteLine("Acesso  Permitido");
+    }
+else
+    {
+        Console.WriteLine("Acesso Negado");
+    }
